d95497067d63001f9c79104e7e68a838b375fa2a
Sean L <slava_be@hotmail.com>
2019-06-25 13:41:56 +0200

    Updated to new CSProj type, and changed TargetFramework of Rant to NETStandard (Everything else to .NET 4.7+, so it'll work 100% with net standard.)
    All tests ran fine 🥰🥰

--- Rant/Properties/AssemblyInfo.cs
+++ Rant/Properties/AssemblyInfo.cs
@@ -26,23 +26,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-#if UNITY
-[assembly: AssemblyTitle("Rant for Unity")]
-#else
-[assembly: AssemblyTitle("Rant")]
-#endif
-[assembly: AssemblyDescription("Rant procedural text generation engine")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rant")]
-[assembly: AssemblyCopyright("Copyright © 2017 Nicholas Fleck")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("Rant.Tools")]
 [assembly: InternalsVisibleTo("Rant.Tests")]
 
@@ -55,16 +38,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("b37e3319-215d-4728-8493-7ae6b70667d9")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("3.0.6")]
